1115887652a40625fdfff68aafd96efab6f411ab
Wesley <serpents123@gmail.com>
2022-06-23 19:15:34 -0700

    Requester removed

--- scavenger.ts
+++ scavenger.ts
@@ -83,24 +83,3 @@ async function weird() {
     await findOperator[i].save();
   }
 }
-
-async function requester() {
-  const test = await fetch('https://gamepress.gg/arknights/tools/interactive-operator-list#tags=null##stats');
-  const $ = load(await test.text());
-  const data: string[] = [];
-  $('.operator-title-actual').each(function(){
-    let name = $(this).text().replaceAll('(', '');
-    name = name.replaceAll(')', '');
-    name = name.replaceAll('the', '');
-    name = name.replaceAll('The', '');
-    name = name.replaceAll("'", '');
-    name = name.replaceAll('.', '');
-    name = name.replaceAll(' ', '-');
-    name = name.replaceAll('---', '-');
-    name = name.replaceAll('--', '-');
-    data.push(name);
-  });
-  const operatorJSON = JSON.stringify(data, null, '\t');
-  fs.writeFile('./operators.json',  operatorJSON, err => {
-    if (err) { console.log("Error writing to file.")}});
-}
