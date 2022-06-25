c79c9dc0380e79cdf83fd1b44eb8499b22d53369
Wesley <serpents123@gmail.com>
2022-06-25 08:58:01 -0700

    Split apart

---
+++ scraper/getCosts.ts
@@ -0,0 +1,34 @@
+import { Page } from "puppeteer";
+import { load } from 'cheerio';
+import fetch from "node-fetch";
+
+export const getCosts = async (page: Page) => {
+  const costNames = await page.evaluate(()=>{
+    const costList: string[] = [];
+    let costLinks: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('#overall-cost a');
+    costLinks.forEach(element => {
+        costList.push(element.href);
+      }
+    );
+    return costList;
+  })
+  const costQuantity = await page.evaluate(()=>{
+    const quantity: string[] = [];
+    let quantities = document.querySelectorAll('#overall-cost .item-qty > span');
+    quantities.forEach(element => {
+      quantity.push(element.textContent!);
+    });
+    return quantity;
+  })
+  for (let i = 0; i < costNames.length; i++) {
+    const url = costNames[i];
+    const test = await fetch(url);
+    const $ = load(await test.text());
+    const name = $('#page-title > h1').text();
+    costNames[i] = name;
+  }
+  const costTable: {[key: string]: string} = {};
+  costNames.forEach((costNames, i) => costTable[costNames] = costQuantity[i]);
+
+  return costTable;
+}
