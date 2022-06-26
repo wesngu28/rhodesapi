5c1a1b77aa12757ea03f1ef565d7477d5568f817
Wesley <serpents123@gmail.com>
2022-06-25 18:30:29 -0700

    Add third route

--- routes/recruitRoute.ts
+++ routes/recruitRoute.ts
@@ -2,6 +2,6 @@ import express from 'express';
 const recruitmentRouter = express.Router()
 import { recruitment } from '../controllers/recruitController'
 
-recruitmentRouter.get('/:tagone&:tagtwo?', recruitment);
+recruitmentRouter.get('/:tagone/:tagtwo?/:tagthree?', recruitment);
 
 export default recruitmentRouter;
