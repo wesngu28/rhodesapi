d83a541194089db4695df4a1b8964e355bc3d596
Wesley <serpents123@gmail.com>
2022-07-05 02:33:07 -0700

    heroku redis

--- models/redis.ts
+++ models/redis.ts
@@ -6,9 +6,15 @@ class Redis {
   public expirationDefault: number;
 
   constructor() {
-    this.client = createClient({
-      url: 'redis://redis:6379'
-      });
+    if(process.env.RUN = 'heroku') {
+      this.client = createClient({
+        url: 'redis://:pb64397616d39c5bed12a4c5734145d384d4f902fc5ecb12378e85c118a834852@ec2-44-206-18-234.compute-1.amazonaws.com:26159'
+        });
+    } else {
+      this.client = createClient({
+        url: 'redis://redis:6379'
+        });
+    }
     this.expirationDefault = 86400000;
   }
 
