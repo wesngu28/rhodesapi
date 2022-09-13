b529cfbe2fb41b73f303ac3579761f8204eafba1
Wesley <60171023+wesngu28@users.noreply.github.com>
2022-09-13 10:02:45 -0700

    test cleanliness

--- util/getCosts.ts
+++ util/getCosts.ts
@@ -7,40 +7,16 @@ export const getCosts = async (url: string) => {
   const response = await fetch(url);
   const text = await response.text();
   const html = parse(text);
-  const costList: {[key: string]: number }= {};
+  let costList: {[key: string]: number }= {};
   let onetoseven = html.querySelectorAll('.skill-material-cost17 a');
   let onetosevenqty = html.querySelectorAll('.skill-material-cost17 .material-quantity');
-  onetoseven.forEach((element, idx) => {
-      const newElement = BASE + element.getAttribute('href');
-      if(!Object.keys(costList).includes(newElement)) {
-          costList[newElement] = Number(onetosevenqty[idx].text.replace('x', ''))
-      } else {
-          costList[newElement] = costList[newElement] + Number(onetosevenqty[idx].text.replace('x', ''))
-      }
-    }
-  );
+  costList = getItemsAndQuantity(costList, onetoseven, onetosevenqty);
   let m1tom3 = html.querySelectorAll('.skill-material-cost810 a');
   let m1tom3qty = html.querySelectorAll('.skill-material-cost810 .material-quantity');
-  m1tom3.forEach((element, idx) => {
-      const newElement = BASE + element.getAttribute('href');
-      if(!Object.keys(costList).includes(newElement)) {
-          costList[newElement] = Number(m1tom3qty[idx].text.replace('x', ''))
-      } else {
-          costList[newElement] = costList[newElement] + Number(m1tom3qty[idx].text.replace('x', ''))
-      }
-    }
-  );
+  costList = getItemsAndQuantity(costList, m1tom3, m1tom3qty);
   let rankcost = html.querySelectorAll('.table-3 a');
   let rankcostqty = html.querySelectorAll('.table-3 .material-quantity');
-  rankcost.forEach((element, idx) => {
-      const newElement = BASE + element.getAttribute('href');
-      if(!Object.keys(costList).includes(newElement)) {
-          costList[newElement] = Number(rankcostqty[idx].text.replace('x', ''))
-      } else {
-          costList[newElement] = costList[newElement] + Number(rankcostqty[idx].text.replace('x', ''))
-      }
-    }
-  );
+  costList = getItemsAndQuantity(costList, rankcost, rankcostqty);
   const newKeys: string[] = [];
   for (let i = 0; i < Object.keys(costList).length; i++) {
       const test = await fetch(Object.keys(costList)[i]);
@@ -49,10 +25,23 @@ export const getCosts = async (url: string) => {
       const name = html.querySelector('#page-title > h1')!.text;
       newKeys.push(name)
   }
-  console.log(newKeys)
   Object.keys(costList).forEach((key, idx) => {
       costList[newKeys[idx]] = costList[key]
       delete costList[key]
   });
+  return costList;
+}
+
+function getItemsAndQuantity(masterList, itemList, qtyList) {
+  const costList = masterList;
+  itemList.forEach((element, idx) => {
+      const newElement = BASE + element.getAttribute('href');
+      if(!Object.keys(costList).includes(newElement)) {
+          costList[newElement] = Number(qtyList[idx].text.replace('x', ''))
+      } else {
+          costList[newElement] = costList[newElement] + Number(qtyList[idx].text.replace('x', ''))
+      }
+    }
+  );
   return costList;
 }
