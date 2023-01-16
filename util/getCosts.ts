cfc23b202773965e06ea1ddd3180b9171317f0d1
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-01-15 22:47:50 -0800

    try/catch for ops on gp without cost or stats yet

--- util/getCosts.ts
+++ util/getCosts.ts
@@ -4,32 +4,36 @@ import { parse } from 'node-html-parser'
 const BASE = 'https://gamepress.gg/'
 
 export const getCosts = async (url: string) => {
-  const response = await fetch(url);
-  const text = await response.text();
-  const html = parse(text);
-  let costList: {[key: string]: number }= {};
-  let onetoseven = html.querySelectorAll('.skill-material-cost17 a');
-  let onetosevenqty = html.querySelectorAll('.skill-material-cost17 .material-quantity');
-  costList = getItemsAndQuantity(costList, onetoseven, onetosevenqty);
-  let m1tom3 = html.querySelectorAll('.skill-material-cost810 a');
-  let m1tom3qty = html.querySelectorAll('.skill-material-cost810 .material-quantity');
-  costList = getItemsAndQuantity(costList, m1tom3, m1tom3qty);
-  let rankcost = html.querySelectorAll('.table-3 a');
-  let rankcostqty = html.querySelectorAll('.table-3 .material-quantity');
-  costList = getItemsAndQuantity(costList, rankcost, rankcostqty);
-  const newKeys: string[] = [];
-  for (let i = 0; i < Object.keys(costList).length; i++) {
-      const test = await fetch(Object.keys(costList)[i]);
-      const text = await test.text();
-      const html = parse(text);
-      const name = html.querySelector('#page-title > h1')!.text;
-      newKeys.push(name)
+  try {
+    const response = await fetch(url);
+    const text = await response.text();
+    const html = parse(text);
+    let costList: {[key: string]: number }= {};
+    let onetoseven = html.querySelectorAll('.skill-material-cost17 a');
+    let onetosevenqty = html.querySelectorAll('.skill-material-cost17 .material-quantity');
+    costList = getItemsAndQuantity(costList, onetoseven, onetosevenqty);
+    let m1tom3 = html.querySelectorAll('.skill-material-cost810 a');
+    let m1tom3qty = html.querySelectorAll('.skill-material-cost810 .material-quantity');
+    costList = getItemsAndQuantity(costList, m1tom3, m1tom3qty);
+    let rankcost = html.querySelectorAll('.table-3 a');
+    let rankcostqty = html.querySelectorAll('.table-3 .material-quantity');
+    costList = getItemsAndQuantity(costList, rankcost, rankcostqty);
+    const newKeys: string[] = [];
+    for (let i = 0; i < Object.keys(costList).length; i++) {
+        const test = await fetch(Object.keys(costList)[i]);
+        const text = await test.text();
+        const html = parse(text);
+        const name = html.querySelector('#page-title > h1')!.text;
+        newKeys.push(name)
+    }
+    Object.keys(costList).forEach((key, idx) => {
+        costList[newKeys[idx]] = costList[key]
+        delete costList[key]
+    });
+    return costList;
+  } catch (err) {
+    return ( { error: 0})
   }
-  Object.keys(costList).forEach((key, idx) => {
-      costList[newKeys[idx]] = costList[key]
-      delete costList[key]
-  });
-  return costList;
 }
 
 function getItemsAndQuantity(masterList: {[key: string]: number }, itemList: any[], qtyList: any[]) {
--- util/getStatistics.ts
+++ util/getStatistics.ts
@@ -9,89 +9,100 @@ type stats = {
 }
 
 export const getStatistics = async (url: string) => {
-  const response = await fetch(url);
-  const text = await response.text();
-  const html = parse(text);
-  const scripts = html.querySelectorAll('script')
-  const stats = [];
-  for (const script of scripts) {
-    if ((script).text.includes('myStats')) {
-      const removals = script.text.substring(0, script.text.indexOf('myStats') + 11)
-      const secremovals = script.text.substring(script.text.indexOf('var summon_stats'), script.text.length)
-      let formattedText = script.text.replace(removals, '');
-      formattedText = formattedText.replace(secremovals, '')
-      formattedText = formattedText.substring(0, formattedText.length - 3)
-      formattedText = parse(formattedText).rawText.replaceAll('\t', '')
-      formattedText = formattedText.replaceAll('"', '')
-      formattedText = formattedText.replaceAll(',', '')
-      let ne = formattedText.substring(0, formattedText.indexOf('}') + 1)
-      formattedText = formattedText.replace(ne, '')
-      const [, ...rest] = ne.split('\n')
-      const statDict = {
-        hp: rest[4].substring(rest[4].indexOf(' ') + 1, rest[4].length),
-        atk: rest[5].substring(rest[5].indexOf(' ') + 1, rest[5].length),
-        def: rest[6].substring(rest[6].indexOf(' ') + 1, rest[6].length),
-        resist: rest[2].substring(rest[1].indexOf(' ') + 1, rest[1].length),
-        deploy: html.querySelectorAll('.other-stat-cell .other-stat-value-cell .effect-description')[1].text.replaceAll('\n', ''),
-        cost: rest[4].substring(rest[2].indexOf(' ') + 1, rest[2].length),
-        interval: html.querySelectorAll('.other-stat-cell .other-stat-value-cell .effect-description')[4].text.replaceAll('\n', ''),
-        block: rest[7].substring(rest[7].indexOf(' ' + 1), rest[7].length)
-      }
-      stats.push(statDict)
-      let max = formattedText.substring(0, formattedText.indexOf('}') + 1)
-      formattedText = formattedText.replace(max, '')
-      const [, ...rests] = max.split('\n')
-      const maxDict = {
-        hp: rests[2].substring(rests[2].indexOf(' ') + 1, rests[2].length),
-        atk: rests[3].substring(rests[3].indexOf(' ') + 1, rests[3].length),
-        def: rests[4].substring(rests[4].indexOf(' ') + 1, rests[4].length),
-        block: rests[5].substring(rests[5].indexOf(' ') + 1, rests[5].length),
-      }
-      stats.push(maxDict)
-      formattedText = formattedText.substring(2, formattedText.length)
-      if (formattedText.includes('e1')) {
-        formattedText = formattedText.replace(formattedText.substring(0, formattedText.indexOf('Max')), '')
-        console.log(formattedText)
-        let ne = formattedText.substring(formattedText.indexOf('Max') + 1, formattedText.indexOf('}') + 1)
-        console.log(ne)
+  try{
+    const response = await fetch(url);
+    const text = await response.text();
+    const html = parse(text);
+    const scripts = html.querySelectorAll('script')
+    const stats = [];
+    for (const script of scripts) {
+      if ((script).text.includes('myStats')) {
+        const removals = script.text.substring(0, script.text.indexOf('myStats') + 11)
+        const secremovals = script.text.substring(script.text.indexOf('var summon_stats'), script.text.length)
+        let formattedText = script.text.replace(removals, '');
+        formattedText = formattedText.replace(secremovals, '')
+        formattedText = formattedText.substring(0, formattedText.length - 3)
+        formattedText = parse(formattedText).rawText.replaceAll('\t', '')
+        formattedText = formattedText.replaceAll('"', '')
+        formattedText = formattedText.replaceAll(',', '')
+        let ne = formattedText.substring(0, formattedText.indexOf('}') + 1)
         formattedText = formattedText.replace(ne, '')
         const [, ...rest] = ne.split('\n')
         const statDict = {
-          hp: rest[1].substring(rest[1].indexOf(' ') + 1, rest[1].length),
-          atk: rest[2].substring(rest[2].indexOf(' ') + 1, rest[2].length),
-          def: rest[3].substring(rest[3].indexOf(' ') + 1, rest[3].length),
-          block: rest[4].substring(rest[4].indexOf(' ') + 1, rest[4].length),
+          hp: rest[4].substring(rest[4].indexOf(' ') + 1, rest[4].length),
+          atk: rest[5].substring(rest[5].indexOf(' ') + 1, rest[5].length),
+          def: rest[6].substring(rest[6].indexOf(' ') + 1, rest[6].length),
+          resist: rest[2].substring(rest[1].indexOf(' ') + 1, rest[1].length),
+          deploy: html.querySelectorAll('.other-stat-cell .other-stat-value-cell .effect-description')[1].text.replaceAll('\n', ''),
+          cost: rest[4].substring(rest[2].indexOf(' ') + 1, rest[2].length),
+          interval: html.querySelectorAll('.other-stat-cell .other-stat-value-cell .effect-description')[4].text.replaceAll('\n', ''),
+          block: rest[7].substring(rest[7].indexOf(' ' + 1), rest[7].length)
         }
         stats.push(statDict)
-        formattedText = formattedText.substring(1, formattedText.length)
-      }
-      if (formattedText.includes('e2')) {
-        formattedText = formattedText.replace(formattedText.substring(0, formattedText.indexOf('Max')), '')
-        console.log(formattedText)
-        let ne = formattedText.substring(formattedText.indexOf('Max') + 1, formattedText.indexOf('}') + 1)
-        console.log(ne)
-        formattedText = formattedText.replace(ne, '')
-        const [, ...rest] = ne.split('\n')
-        const statDict = {
-          hp: rest[1].substring(rest[1].indexOf(' ') + 1, rest[1].length),
-          atk: rest[2].substring(rest[2].indexOf(' ') + 1, rest[2].length),
-          def: rest[3].substring(rest[3].indexOf(' ') + 1, rest[3].length),
-          block: rest[4].substring(rest[4].indexOf(' ') + 1, rest[4].length),
+        let max = formattedText.substring(0, formattedText.indexOf('}') + 1)
+        formattedText = formattedText.replace(max, '')
+        const [, ...rests] = max.split('\n')
+        const maxDict = {
+          hp: rests[2].substring(rests[2].indexOf(' ') + 1, rests[2].length),
+          atk: rests[3].substring(rests[3].indexOf(' ') + 1, rests[3].length),
+          def: rests[4].substring(rests[4].indexOf(' ') + 1, rests[4].length),
+          block: rests[5].substring(rests[5].indexOf(' ') + 1, rests[5].length),
         }
-        stats.push(statDict)
-        formattedText = formattedText.substring(1, formattedText.length)
+        stats.push(maxDict)
+        formattedText = formattedText.substring(2, formattedText.length)
+        if (formattedText.includes('e1')) {
+          formattedText = formattedText.replace(formattedText.substring(0, formattedText.indexOf('Max')), '')
+          console.log(formattedText)
+          let ne = formattedText.substring(formattedText.indexOf('Max') + 1, formattedText.indexOf('}') + 1)
+          console.log(ne)
+          formattedText = formattedText.replace(ne, '')
+          const [, ...rest] = ne.split('\n')
+          const statDict = {
+            hp: rest[1].substring(rest[1].indexOf(' ') + 1, rest[1].length),
+            atk: rest[2].substring(rest[2].indexOf(' ') + 1, rest[2].length),
+            def: rest[3].substring(rest[3].indexOf(' ') + 1, rest[3].length),
+            block: rest[4].substring(rest[4].indexOf(' ') + 1, rest[4].length),
+          }
+          stats.push(statDict)
+          formattedText = formattedText.substring(1, formattedText.length)
+        }
+        if (formattedText.includes('e2')) {
+          formattedText = formattedText.replace(formattedText.substring(0, formattedText.indexOf('Max')), '')
+          console.log(formattedText)
+          let ne = formattedText.substring(formattedText.indexOf('Max') + 1, formattedText.indexOf('}') + 1)
+          console.log(ne)
+          formattedText = formattedText.replace(ne, '')
+          const [, ...rest] = ne.split('\n')
+          const statDict = {
+            hp: rest[1].substring(rest[1].indexOf(' ') + 1, rest[1].length),
+            atk: rest[2].substring(rest[2].indexOf(' ') + 1, rest[2].length),
+            def: rest[3].substring(rest[3].indexOf(' ') + 1, rest[3].length),
+            block: rest[4].substring(rest[4].indexOf(' ') + 1, rest[4].length),
+          }
+          stats.push(statDict)
+          formattedText = formattedText.substring(1, formattedText.length)
+        }
       }
     }
+    const statistics: stats = {
+      base: stats[0],
+      e0max: stats[1]
+    }
+    if(stats[2].hp) {
+      statistics['e1max'] = stats[2]
+    }
+    if(stats[3].hp) {
+      statistics['e2max'] = stats[3]
+    }
+    return statistics;
+  } catch (err) {
+    return (
+      {
+        base: {error: "something went wrong, most likely operator does not have stats yet on gp"},
+        e0max: {error: "something went wrong, most likely operator does not have stats yet on gp"},
+        e1max: {error: "something went wrong, most likely operator does not have stats yet on gp"},
+        e2max: {error: "something went wrong, most likely operator does not have stats yet on gp"},
+      }
+    )
   }
-  const statistics: stats = {
-    base: stats[0],
-    e0max: stats[1]
-  }
-  if(stats[2].hp) {
-    statistics['e1max'] = stats[2]
-  }
-  if(stats[3].hp) {
-    statistics['e2max'] = stats[3]
-  }
-  return statistics;
 }
