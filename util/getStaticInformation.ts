99ba9a8674ffa7751610848b144a52ca464f76a9
Wesley <60171023+wesngu28@users.noreply.github.com>
2023-03-14 10:55:38 -0700

    revert e2 base renaming

--- util/getStaticInformation.ts
+++ util/getStaticInformation.ts
@@ -258,8 +258,19 @@ export const getStaticInformation = async (url: string) => {
     const compare = Object.keys(operatorArt);
     for (let i = 0; i < imgLinkList.length; i++) {
       operatorArt[compare[i]] = 'https://gamepress.gg' + operatorArt[compare[i]];
+      if(rarity > 2 && i === 0) {
+        delete operatorArt[compare[i]];
+      }
       if(rarity < 2 && i === 0) {
         operatorArt['Base'] = operatorArt[compare[i]];
+        delete operatorArt[compare[i]];
+      }
+      if(rarity > 2 && i === 1) {
+        operatorArt['Base'] = operatorArt[compare[i]];
+        delete operatorArt[compare[i]];
+      }
+      if(compare[i].includes('https') && i === 2) {
+        operatorArt['E2'] = operatorArt[compare[i]];
         delete operatorArt[compare[i]];
       }
       if (!compare[i].includes('png')) {
@@ -271,6 +282,7 @@ export const getStaticInformation = async (url: string) => {
         delete operatorArt[compare[i]];
       }
     }
+    console.log(operatorArt)
     function checkForExistence(field: any) : string {
       if (field) {
         field = field.text().replace(/\n/g, '');
