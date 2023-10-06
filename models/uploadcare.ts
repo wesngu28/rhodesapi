6d27318811ab7e2bca94367e88d1c49847a718a3
wesngu28 <wesley@wesngu28.com>
2023-10-05 22:18:40 -0700

    fix: wrong key for secret var

--- models/uploadcare.ts
+++ models/uploadcare.ts
@@ -2,5 +2,5 @@ import { UploadcareSimpleAuthSchema } from "@uploadcare/rest-client";
 
 export const uploadCareSimpleAuthSchema = new UploadcareSimpleAuthSchema({
     publicKey: 'e4e7900bd16b1b5b3363',
-    secretKey: process.env.REDIS_PASSWORD
+    secretKey: process.env.UPLOADCARE_SECRET
 })
